95fed35ed051eff1baaf229eb898d8b09db0fe11
najibu <ns.najibu@gmail.com>
2017-01-21 19:44:33 +0000

    Update index view

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -10,8 +10,9 @@ class TasksController extends Controller
 {
     public function index()
     {
-      
-      return view('tasks.index');
+      $tasks = Task::all();
+
+      return view('tasks.index')->withTasks($tasks);
     }
 
     public function create()
