da92186e3614f81aa21eed01e0c69474437e6659
najibu <ns.najibu@gmail.com>
2017-01-21 19:25:50 +0000

    Removed illuminate

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,4 +4,22 @@
   <h1>Add a new Task</h1>
 
   <p class="lead">Add to your task list below.</p>
+
+  {!! Form::open([
+    'route' => 'tasks.store'
+]) !!}
+
+<div class="form-group">
+    {!! Form::label('title', 'Title:', ['class' => 'control-label']) !!}
+    {!! Form::text('title', null, ['class' => 'form-control']) !!}
+</div>
+
+<div class="form-group">
+    {!! Form::label('description', 'Description:', ['class' => 'control-label']) !!}
+    {!! Form::textarea('description', null, ['class' => 'form-control']) !!}
+</div>
+
+{!! Form::submit('Create New Task', ['class' => 'btn btn-primary']) !!}
+
+{!! Form::close() !!}
 @stop
