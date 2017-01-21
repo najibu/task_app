11beb81258a27aae3436c717e8a47dafafb561f7
najibu <ns.najibu@gmail.com>
2017-01-21 19:38:57 +0000

    Added create task view

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -2,17 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Task;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class TasksController extends Controller
 {
     public function index()
     {
+      
       return view('tasks.index');
     }
 
     public function create()
     {
       return view('tasks.create');
+    }
+
+    public function store(Request $request)
+    {
+      $this->validate($request, [
+        'title' => 'required',
+        'description' => 'required'
+      ]);
+
+      $input = $request->all();
+
+      Task::create($input);
+
+      Session::flash('flash_message', 'Task successfully added!');
+
+      return redirect()->back();
     }
 }
--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -7,19 +7,33 @@
 
   {!! Form::open([
     'route' => 'tasks.store'
-]) !!}
+  ]) !!}
 
-<div class="form-group">
-    {!! Form::label('title', 'Title:', ['class' => 'control-label']) !!}
-    {!! Form::text('title', null, ['class' => 'form-control']) !!}
-</div>
+  @if($errors->any())
+    <div class="alert alert-danger">
+      @foreach($errors->all() as $error)
+        <p>{{ $error }}</p>
+      @endforeach
+    </div>
+  @endif
 
-<div class="form-group">
-    {!! Form::label('description', 'Description:', ['class' => 'control-label']) !!}
-    {!! Form::textarea('description', null, ['class' => 'form-control']) !!}
-</div>
+  @if(Session::has('flash_message'))
+    <div class="alert alert-success">
+      {{ Session::get('flash_message') }}
+    </div>
+  @endif
 
-{!! Form::submit('Create New Task', ['class' => 'btn btn-primary']) !!}
+  <div class="form-group">
+      {!! Form::label('title', 'Title:', ['class' => 'control-label']) !!}
+      {!! Form::text('title', null, ['class' => 'form-control']) !!}
+  </div>
 
-{!! Form::close() !!}
+  <div class="form-group">
+      {!! Form::label('description', 'Description:', ['class' => 'control-label']) !!}
+      {!! Form::textarea('description', null, ['class' => 'form-control']) !!}
+  </div>
+
+  {!! Form::submit('Create New Task', ['class' => 'btn btn-primary']) !!}
+
+  {!! Form::close() !!}
 @stop
